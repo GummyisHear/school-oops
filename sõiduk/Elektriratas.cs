ca4b55a58ef7dfdcf52dcda1843d9559055d3019
GummyisHear <artjompoldsaar@gmail.com>
2025-09-19 11:40:17 +0300

    elektriratas

---
+++ sõiduk/Elektriratas.cs
@@ -0,0 +1,28 @@
+﻿namespace oops.sõiduk;
+
+internal class Elektriratas : ISoiduk
+{
+    public double Kütusekulu;
+    public double Km;
+
+    public Elektriratas(double kutusekulu, double km)
+    {
+        Kütusekulu = kutusekulu;
+        Km = km;
+    }
+
+    public double ArvutaKulu()
+    {
+        return Kütusekulu;
+    }
+
+    public double ArvutaVahemaa()
+    {
+        return Km;
+    }
+
+    public string Export()
+    {
+        return $"Elektriratas, {Kütusekulu}, {Km}";
+    }
+}
